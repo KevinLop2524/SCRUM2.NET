8a29d94376535677b53744e80ab6b0ef40f8a41c
nicolastabares <tabaresnicolas855@gmail.com>
2025-11-02 17:17:42 -0500

    Renovacion de Carpeta Principal

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCRUM2NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
